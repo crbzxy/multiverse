5cc1b206e3c56b5ce2eba74e983cb0ccd8f85547
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-03-09 21:40:26 -0600

    ajuste del package,json

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+<h1>Hola Mundo</h1>        
+<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
