c8b9fe4f1107dea6d8579078b3971577ac014559
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-04-11 03:14:42 -0500

    a mimir

--- src/views/Galeria.js
+++ src/views/Galeria.js
@@ -1,7 +1,6 @@
 import React from "react";
 import prevportal from "../img/Prevportal.png";
 
-
 require("aframe");
 require("aframe-environment-component");
 require("aframe-liquid-portal-shader");
@@ -17,14 +16,21 @@ const Hueso = () => {
       <div className="scene">
         <>
           {" "}
-          <a-scene>
-          <a-camera position="3 7 2">
-              <a-cursor material="color: green" cursor="fuse: false; fuseTimeout: 5000"></a-cursor>
+          <a-scene background="color: red">
+            <a-camera position="3 7 2">
+              <a-cursor
+                material="color: green"
+                cursor="fuse: false; fuseTimeout: 1000"
+              ></a-cursor>
             </a-camera>
             <a-assets>
               {/* <a-asset-item id="cristal" src={cristal}></a-asset-item> */}
-              <img id="demo" alt="hola" src={prevportal} crossorigin="anonymous" />
-              
+              <img
+                id="demo"
+                alt="hola"
+                src={prevportal}
+                crossorigin="anonymous"
+              />
             </a-assets>
 
             <a-entity
@@ -33,7 +39,6 @@ const Hueso = () => {
               shadow="receive: true"
             />
 
-           
             {/* hoja de usuario */}
             {/* <a-plane
               position="2.3 8 -10"
@@ -45,40 +50,37 @@ const Hueso = () => {
             <a-link
               href="https://crbzxy.github.io/helloworld/"
               title="Errar-Possibility of light"
-              scale="5 5 5"
-              position="-1 11 -30"
+              scale="6 6 6"
+              position="-1 8 -30"
               image="#demo"
+              borderColor="black"
               event-set__enter="_event: mouseenter; _target: #demo; visible: true"
-                event-set__leave="_event: mouseleave; _target: #demo; visible: false"
+              event-set__leave="_event: mouseleave; _target: #demo; visible: false"
             >
-
-            <a-image
-              src="#demo"
-              material="shader: liquid-portal"
-              scale="2 2 2"
-              position="0 0 0.01"
-                       />
+              <a-image
+                src="#demo"
+                material="shader: liquid-portal"
+                scale="2 2 2"
+                position="0 0 0.01"
+              />
             </a-link>
 
             <a-link
               href="https://crbzxy.github.io/xyz/"
               title="xyz"
-              scale="5 5 5"
-              position="30 11 -30"
+              scale="6 6 6"
+              position="30 8 -30"
               image="#demo"
               backgroundColor="blue"
             >
-
-            <a-image
-              src="#demo"
-              material="shader: liquid-portal"
-              scale="2 2 2"
-              backgroundColor="blue"
-              position="0 0 0.01"
-                       />
+              <a-image
+                src="#demo"
+                material="shader: liquid-portal"
+                scale="2 2 2"
+                backgroundColor="blue"
+                position="0 0 0.01"
+              />
             </a-link>
-
-            
           </a-scene>
         </>
       </div>
