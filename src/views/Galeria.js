1c47009d2088d89189aaedd8b7fe9df868ded567
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-04-11 01:53:06 -0500

    avance

--- src/views/Galeria.js
+++ src/views/Galeria.js
@@ -1,8 +1,11 @@
 import React from "react";
+import prevportal from "../img/Prevportal.png";
 
-import galeria from "../models/galeria.gltf";
+
 require("aframe");
 require("aframe-environment-component");
+require("aframe-liquid-portal-shader");
+require("aframe-event-set-component");
 
 // import campo from "../img/equ.jpg";
 
@@ -15,23 +18,42 @@ const Hueso = () => {
         <>
           {" "}
           <a-scene>
-            <a-entity
-              environment="preset: default; fog:0.7; groundColor:white; groundColor2:#fcfcfc"
-              scale="2 1 2"/>
-            
-            <a-camera position="3 7 2"></a-camera>
-            <a-plane position="1.5 8 -10" rotation="0 0 0" scale="20 18 0"/>
-            
             <a-assets>
-              <a-asset-item id="galeria" src={galeria}></a-asset-item>
+              {/* <a-asset-item id="cristal" src={cristal}></a-asset-item> */}
+              <img id="demo" alt="hola" src={prevportal} crossorigin="anonymous" />
+              
             </a-assets>
 
             <a-entity
-              gltf-model={galeria}
-              scale="5 8 5"
-              position="0 0 0"
-             
-              shadow="receive: false"
+              environment="preset: default; castShadow: true; fog:0.7; groundColor:white; groundColor2:#fcfcfc"
+              scale="2 1 2"
+              shadow="receive: true"
+            />
+
+            <a-camera position="3 7 2">
+              <a-cursor></a-cursor>
+            </a-camera>
+            {/* hoja de usuario */}
+            {/* <a-plane
+              position="2.3 8 -10"
+              rotation="0 0 0"
+              scale="20 15 2"
+              
+            /> */}
+
+            <a-link
+              href="https://crbzxy.github.io/helloworld/"
+              title="#demo"
+              image="#demo"
+              position="-1 11 -30"
+              
+            />
+
+            <a-image
+              src="#demo"
+              material="shader: liquid-portal"
+              scale="20 20 20"
+              position="-1 11 -30"
             />
           </a-scene>
         </>
